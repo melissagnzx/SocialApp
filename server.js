a4b504bd9ba92c89266b52420ffbea0021a971bf
gonzalm7 <gonzalm7@wwu.edu>
2019-10-26 16:42:10 -0700

    added db config files

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+//Connect DB
+connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
